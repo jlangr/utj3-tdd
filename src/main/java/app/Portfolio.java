f47e46c999ad838b514b8da3a96a663a42ff38c5
Jeff L <jeff@langrsoft.com>
2024-03-30 07:01:45 -0600

    passing not purchased

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -1,42 +1,29 @@
 package app;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
-// START:impl
 public class Portfolio {
-    // START_HIGHLIGHT
     private Map<String, Integer> purchases = new HashMap<>();
-    // END_HIGHLIGHT
-    private Set symbols = new HashSet<String>();
-    private int shares;
 
     public boolean isEmpty() {
-        // START_HIGHLIGHT
         return purchases.isEmpty();
-        // END_HIGHLIGHT
     }
 
     public void purchase(String symbol, int shares) {
-        symbols.add(symbol);
-        this.shares = shares;
-        // START_HIGHLIGHT
         purchases.put(symbol, shares);
-        // END_HIGHLIGHT
     }
 
     public int size() {
-        // START_HIGHLIGHT
         return purchases.size();
-        // END_HIGHLIGHT
     }
 
     public int sharesOf(String symbol) {
         // START_HIGHLIGHT
-        return purchases.get(symbol);
+        if (!purchases.containsKey(symbol)) return 0;
         // END_HIGHLIGHT
+
+        return purchases.get(symbol);
     }
 }
 // END:impl
