8183acdf670a9103651d8a4853f9130b9860f451
Jeff L <jeff@langrsoft.com>
2024-03-30 06:26:52 -0600

    remove int

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -4,15 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Portfolio {
-    private int size = 0;
     private Set symbols = new HashSet<String>();
 
     public boolean isEmpty() {
-        return size == 0;
+        return symbols.isEmpty();
     }
 
     public void purchase(String symbol, int shares) {
-        size++;
         symbols.add(symbol);
     }
 
