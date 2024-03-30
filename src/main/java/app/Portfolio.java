c0ee52107ea10107db2793a7ef56a475086c1ce4
Jeff L <jeff@langrsoft.com>
2024-03-29 21:23:09 -0600

    pass

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -1,10 +1,13 @@
 package app;
 
 public class Portfolio {
+    private boolean isEmpty = true;
+
     public boolean isEmpty() {
-        return true;
+        return isEmpty;
     }
 
     public void purchase(String symbol, int shares) {
+        isEmpty = false;
     }
 }
