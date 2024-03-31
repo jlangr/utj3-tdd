3a77b9b0ac32c452c54f743637f80c6ec36c2fdc
Jeff L <jeff@langrsoft.com>
2024-03-31 13:35:38 -0600

    fix highlights

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -3,22 +3,30 @@ package app;
 import java.util.HashSet;
 import java.util.Set;
 
+// START:impl
 public class Portfolio {
     private int size = 0;
+    // START_HIGHLIGHT
     private Set symbols = new HashSet<String>();
+    // END_HIGHLIGHT
 
     public boolean isEmpty() {
-        return size == 0;
+        // START_HIGHLIGHT
+        return symbols.isEmpty();
+        // END_HIGHLIGHT
     }
 
     public void purchase(String symbol, int shares) {
         size++;
+        // START_HIGHLIGHT
         symbols.add(symbol);
+        // END_HIGHLIGHT
     }
 
-    // START:impl
     public int size() {
+        // START_HIGHLIGHT
         return symbols.size();
+        // END_HIGHLIGHT
     }
-    // END:impl
 }
+// END:impl
