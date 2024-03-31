dfca90f2dd5f6b613289f203118851d0b62a3ed4
Jeff L <jeff@langrsoft.com>
2024-03-31 14:55:48 -0600

    highlights

--- src/main/java/app/Portfolio.java
+++ src/main/java/app/Portfolio.java
@@ -3,26 +3,35 @@ package app;
 import java.util.HashSet;
 import java.util.Set;
 
+// START:impl
 public class Portfolio {
     private Set symbols = new HashSet<String>();
+    // START_HIGHLIGHT
     private int shares;
+    // END_HIGHLIGHT
 
+    // ...
+    // STOP:impl
     public boolean isEmpty() {
         return symbols.isEmpty();
     }
 
-    public void purchase(String symbol, int shares) {
-        symbols.add(symbol);
-        this.shares = shares;
-    }
-
-    // START:impl
     public int size() {
         return symbols.size();
     }
 
+    // START:impl
+    public void purchase(String symbol, int shares) {
+        symbols.add(symbol);
+        // START_HIGHLIGHT
+        this.shares = shares;
+        // END_HIGHLIGHT
+    }
+
+    // START_HIGHLIGHT
     public int sharesOf(String symbol) {
         return shares;
     }
-    // END:impl
+    // END_HIGHLIGHT
 }
+// END:impl
