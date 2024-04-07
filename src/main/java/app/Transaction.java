4721860e0083db1efd500cffeea90600171cde1e
Jeff L <jeff@langrsoft.com>
2024-04-06 19:20:57 -0600

    fix line lengths Transaction

--- src/main/java/app/Transaction.java
+++ src/main/java/app/Transaction.java
@@ -3,6 +3,7 @@ package app;
 // START:impl
 import java.time.Instant;
 
-public record Transaction(String symbol, int shares, TransactionType type, Instant now) {
+public record Transaction(
+    String symbol, int shares, TransactionType type, Instant now) {
 }
 // END:impl
