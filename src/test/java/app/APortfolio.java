9c6e9da71dfe64c50e09b642985dfada6e1a43ad
Jeff Langr <jeff@langrsoft.com>
2025-04-16 08:36:40 -0600

    swap asserts order, thanks demmer12

--- src/test/java/app/APortfolio.java
+++ src/test/java/app/APortfolio.java
@@ -132,8 +132,8 @@ public class APortfolio {
         void returnsLastTransactionAfterPurchase() {
             portfolio.purchase("SONO", 20);
 
-            assertEquals(portfolio.lastTransaction(),
-                new Transaction("SONO", 20, BUY, now));
+            assertEquals(new Transaction("SONO", 20, BUY, now),
+               portfolio.lastTransaction());
         }
         // END:test
 
